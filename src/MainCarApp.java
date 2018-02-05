2daf3cc425485bcc66bf0e78db1a6eff236c0888
malarbw <35358615+malarbw@users.noreply.github.com>
2018-02-05 09:15:57 -0500

    Update MainCarApp.java

--- src/MainCarApp.java
+++ src/MainCarApp.java
@@ -1,16 +1,21 @@
+/*
+*Brad Malarkey
+*
+*Lab 10 -- extend car class with used car class, if user picks car from inventory remove car from list
+*/
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainCarApp {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in); //initialize all the variables and my array list to hold the cars
 		int userChoice;
 		String confirm;
 		String cont = "y";
 		ArrayList<Cars> inventory = new ArrayList<Cars>();
 
-		inventory.add(new Cars("Ford", "Explorer", 2013, 16000.00));
+		inventory.add(new Cars("Ford", "Explorer", 2013, 16000.00)); //add car options
 		inventory.add(new Cars("Toyota", "Camry", 2017, 26000.00));
 		inventory.add(new Cars("Dodge", "Charger", 2016, 19000.00));
 		inventory.add(new UsedCars("Ford", "Focus", 2014, 12000.00, 30246.02));
@@ -21,19 +26,19 @@ public class MainCarApp {
 		while (cont.equalsIgnoreCase("y")) {
 			
 		
-		for (Cars i : inventory) {
+		for (Cars i : inventory) { //prints out inventory
 			System.out.println(i);
 
 		}
 		userChoice = Validator.getInt(scan, "Which car would you like? (Enter corresponding number): ", 1, 6);
 
-		switch (userChoice) {
+		switch (userChoice) { //used switch case since I knew the options the user had to choose from
 		case 1:
 			System.out.println(inventory.get(0));
-			System.out.println("Would you like to buy this car? (y/n): ");
+			System.out.println("Would you like to buy this car? (y/n): "); 
 			confirm = scan.nextLine();
 			if (confirm.equalsIgnoreCase("y")) {
-				inventory.remove(0);
+				inventory.remove(0); //removes car if user wants it
 				System.out.println("Excellent! Our finance deparment will be in touch shortly.");
 			}
 			break;
